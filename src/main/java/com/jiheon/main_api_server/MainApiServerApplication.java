6c08097c9549f03da9128f09dde188cecf4dcbc4
JHeon123 <jiheon340@gmail.com>
2026-07-31 15:52:34 +0900

    first commit

---
+++ src/main/java/com/jiheon/main_api_server/MainApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.jiheon.main_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiheon/main_api_server/MainApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiheon.main_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
